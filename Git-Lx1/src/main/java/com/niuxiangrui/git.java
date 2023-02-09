72c2a8ead73bd2c0c7afbf087605454f9bed47ff
XRUI <2223103391@qq.com>
2023-02-09 11:57:56 +0800

    second commit

--- Git-Lx1/src/main/java/com/niuxiangrui/git.java
+++ Git-Lx1/src/main/java/com/niuxiangrui/git.java
@@ -9,5 +9,6 @@ package com.niuxiangrui;
 public class git {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
