53c9078aa602cbaba7d06459644077990d1a48b0
XRUI <2223103391@qq.com>
2023-02-09 11:55:08 +0800

    fitst commit

---
+++ Git-Lx1/src/main/java/com/niuxiangrui/git.java
@@ -0,0 +1,13 @@
+package com.niuxiangrui;
+
+/**
+ * @author NXRUI
+ * @Classname git
+ * @Version 1.0
+ * @Date 2023/2/9 11:49
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
